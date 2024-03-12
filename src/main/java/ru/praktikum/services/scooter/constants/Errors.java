748e8c9ed5eaf72f28c26b4e919262fb961b2af9
Yaroslav Erofeev <yaroslaveerofeev@yandex.ru>
2024-03-12 13:22:02 +0300

    Поправил текст ошибки

--- src/main/java/ru/praktikum/services/scooter/constants/Errors.java
+++ src/main/java/ru/praktikum/services/scooter/constants/Errors.java
@@ -1,7 +1,7 @@
 package ru.praktikum.services.scooter.constants;
 
 public class Errors {
-    public static final String TWO_IDENTICAL_COURIERS = "Этот логин уже используется";
+    public static final String TWO_IDENTICAL_COURIERS = "Этот логин уже используется. Попробуйте другой.";
     public static final String WITHOUT_PASS_LOGIN = "Недостаточно данных для создания учетной записи";
     public static final String LOGIN_WITHOUT_PASS_LOGIN = "Недостаточно данных для входа";
     public static final String NON_EXISTENT_LOGIN = "Учетная запись не найдена";
